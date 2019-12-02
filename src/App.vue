f62ee69d62363541b3f842df700bc6a3964611d4
Kevindm14 <kevindiazm.14@gmail.com>
2019-12-01 19:23:24 -0500

    add tasks

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tasks/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Tasks
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #455A64;
+}
+
 #app {
+  width: 100%;
+  max-width: 45rem;
+  border-radius: 5px;
+  padding: 1em;
+  margin: 1em auto;
+  overflow: auto;
+  background-color: white;
+  box-shadow: 0px .25rem 1rem rgba(black, .25);
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
